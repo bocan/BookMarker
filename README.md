0f3381e0af918981211d8cc1792fe30b212e00c3
Chris Funderburg <c.funderburg@mak-system.net>
2025-05-22 15:16:33 +0100

    add code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BookMarker
 A static website meant to be access locally via a file:/// link.  It just saves bookmarks for me.
+
+I, for instance, check this out to a folder called "bookmarks" under my home directory on my Macbook.  Then I open my browser and go to file:///Users/chris/bookmarks/index.html
---
+++ metadata.js
@@ -0,0 +1,105 @@
+const bookmarks = [
+  {
+    "url": "https://chris.funderburg.me/",
+    "title": "Chris Funderburg's Blog / Home Page",
+    "description": "My home page, where I share my thoughts and idea.",
+    "tags": ["home", "blog", "me"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://joshcollinsworth.com/blog/fwiw",
+    "title": "Advice on Job Hunting in Tech",
+    "description": "A blog post by Josh Collinsworth sharing his thoughts and advice on job hunting in the tech industry.",
+    "tags": ["job-hunting", "tech", "advice"],
+    "publicationYear": "2024"
+  },
+  {
+    "url": "https://supremecbd.uk/",
+    "title": "Supreme CBD",
+    "tags": ["cbd", "medicine", "health"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://www.thebromptonman.com/torque-settings.html",
+    "title": "Brompton Torque Settings",
+    "description": "A comprehensive guide to the torque settings for various components of a Brompton bicycle.",
+    "tags": ["cycling", "brompton", "maintenance"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://www.hockerty.uk/en-uk/",
+    "title": "Hockerty - Custom Tailored Clothing",
+    "description": "Hockerty offers custom tailored clothing. You can design your own clothes online and have them made to your specifications. They provide a wide range of options for suits, shirts, and other garments.",
+    "tags": ["fashion", "clothing", "tailoring"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://medium.com/@vvbvargas/how-i-built-my-kubernetes-cluster-with-a-shared-storage-on-raspberry-pi-using-k3s-1c06b424df97",
+    "title": "How I built my Kubernetes cluster with a shared storage on Raspberry Pi using K3s",
+    "tags": ["kubernetes", "raspberry-pi"],
+    "publicationYear": "2023"
+  },
+  {
+    "url": "https://github.com/sindresorhus/awesome",
+    "title": "Awesome Lists",
+    "description": "😎 Awesome lists about all kinds of interesting topics.",
+    "tags": ["awesome", "tech"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://github.com/donnemartin/awesome-aws",
+    "title": "Awesome AWS",
+    "description": "A curated list of awesome Amazon Web Services (AWS) libraries, open source repos, guides, blogs, and other resources. Featuring the Fiery Meter of AWSome.",
+    "tags": ["awesome", "aws", "tech"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://github.com/wmariuss/awesome-devops",
+    "title": "Awesome DevOps",
+    "description": "A curated list of awesome DevOps platforms, tools, practices and resources.",
+    "tags": ["awesome", "devops", "tech"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://www.udemy.com/home/my-courses/learning/",
+    "title": "Udemy - My Courses",
+    "description": "A collection of my courses on Udemy.",
+    "tags": ["udemy", "courses", "training"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://techworld-with-nana.teachable.com/courses/enrolled/2186952",
+    "title": "TechWorld with Nana - Courses",
+    "description": "A collection of courses on DevOps and cloud computing.",
+    "tags": ["techworld", "courses", "devops", "training"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://superorbital.io/blog/aws-auth-and-kubernetes-deep-dive/",
+    "title": "AWS Auth and Kubernetes Deep Dive",
+    "description": "A deep dive into AWS authentication and Kubernetes.",
+    "tags": ["tech", "aws", "kubernetes", "auth"],
+    "publicationYear": "2024"
+  },
+  {
+    "url": "https://earth.nullschool.net/",
+    "title": "Earth Nullschool",
+    "description": "A global map of wind, weather, and ocean conditions. It provides a visual representation of the Earth's atmosphere and ocean currents.",
+    "tags": ["weather", "earth", "maps"],
+    "publicationYear": "2025"
+  },
+  {
+    "url": "https://www.lightningmaps.org/#m=oss;t=3;s=0;o=0;b=;ts=0;",
+    "title": "Lightning Maps",
+    "description": "A real-time map of lightning strikes around the world. You can see where lightning is striking and how it moves.",
+    "tags": ["weather", "lightning", "maps"],
+    "publicationYear": "2025"
+  },
+ // {
+ //   "url": "",
+ //   "title": "",
+ //   "description": "",
+ //   "tags": [""],
+ //   "publicationYear": ""
+ // },
+];
